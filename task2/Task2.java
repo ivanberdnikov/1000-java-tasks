00b0c4509b4039edda2b404c811b498d5a1ef5dc
Ivan <mr.berdnikov1998@mail.ru>
2023-02-06 19:58:27 +0300

    Task 2 solved

---
+++ task2/Task2.java
@@ -0,0 +1,12 @@
+package task2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
