a8363358faf021386d7d4fb082e946ad5a54125f
Ivan <mr.berdnikov1998@mail.ru>
2023-02-07 07:22:02 +0300

    Task 7 solved

---
+++ task7/Task7.java
@@ -0,0 +1,13 @@
+package task7;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(n * a * b * 2);
+    }
+}
